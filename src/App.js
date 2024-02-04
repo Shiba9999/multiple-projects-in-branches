17ed9afae20857230f600a9f0217ad15a3d749d3
shiba <rp.shiba2@gmail.com>
2024-02-05 01:19:01 +0530

    progressbar done

--- src/App.js
+++ src/App.js
@@ -1,62 +1,18 @@
 import React, { useEffect, useState } from "react";
+import ProgressBar from "./components/ProgressBar";
 import "./App.css";
 
 const App = () => {
-  const [products, setProducts] = useState([]);
-  const [page, setPage] = useState(1);
-  const fetchProducts = async () => {
-    const res = await fetch(`https://dummyjson.com/products?limit=100`);
-    const data = await res.json();
-    if (data && data.products) {
-      setProducts(data.products);
-    }
-  };
+  const [value, setValue] = useState(0);
   useEffect(() => {
-    fetchProducts();
+    setInterval(() => {
+      setValue((val) => val + 1);
+    }, 100);
   }, []);
-  if (products.length > 0) {
-    console.log([...Array(products.length / 10)]);
-  }
-  function selectPageHandler(selectedPage) {
-    if (
-      selectedPage >= 1 &&
-      selectedPage <= products.length / 10 
-    ) {
-      setPage(selectedPage);
-    }
-  }
   return (
-    <div>
-      {products.length > 0 && (
-        <div className="products">
-          {products.slice(page * 10 - 10, page * 10).map((prod) => (
-            <span key={prod.id} className="products__single">
-              <img src={prod.thumbnail} alt={prod.title} />
-              <p>{prod.title}</p>
-            </span>
-          ))}
-        </div>
-      )}
-
-      {products.length > 0 && (
-        <div className="pagination">
-          <span
-           className={page>1 ? "" :"pagination__disabled"}
-          onClick={() => selectPageHandler(page - 1)}>👈</span>
-          {[...Array(products.length / 10)].map((_, i) => (
-            <span
-              className={page === i + 1 ? "pagination__selected" : ""}
-              onClick={() => selectPageHandler(i + 1)}
-              key={i}
-            >
-              {i + 1}
-            </span>
-          ))}
-          <span 
-          className={page < products.length/10 ? "" :"pagination__disabled"}
-          onClick={() => selectPageHandler(page + 1)}>👉</span>
-        </div>
-      )}
+    <div className="app">
+      <span>Progress Bar</span>
+      <ProgressBar value={value} />
     </div>
   );
 };
