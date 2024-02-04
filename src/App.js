a6a8c5c3ce2889fa9fd147b440fb9bec478f7fbc
shiba <rp.shiba2@gmail.com>
2024-02-04 16:16:00 +0530

    pagination

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Blog from "./components/Blog";
-import About from "./components/About";
-import Home from "./components/Home";
-import Navbar from "./components/Navbar";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import { ThemeProvider } from "./theme-context";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const fetchProducts = async () => {
+    const res = await fetch(`https://dummyjson.com/products?limit=100`);
+    const data = await res.json();
+    if (data && data.products) {
+      setProducts(data.products);
+    }
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  if (products.length > 0) {
+    console.log([...Array(products.length / 10)]);
+  }
+  function selectPageHandler(selectedPage) {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= products.length / 10 
+    ) {
+      setPage(selectedPage);
+    }
+  }
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/blog" element={<Blog />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <div>
+      {products.length > 0 && (
+        <div className="products">
+          {products.slice(page * 10 - 10, page * 10).map((prod) => (
+            <span key={prod.id} className="products__single">
+              <img src={prod.thumbnail} alt={prod.title} />
+              <p>{prod.title}</p>
+            </span>
+          ))}
+        </div>
+      )}
+
+      {products.length > 0 && (
+        <div className="pagination">
+          <span onClick={() => selectPageHandler(page - 1)}>👈</span>
+          {[...Array(products.length / 10)].map((_, i) => (
+            <span
+              className={page === i + 1 ? "pagination__selected" : ""}
+              onClick={() => selectPageHandler(i + 1)}
+              key={i}
+            >
+              {i + 1}
+            </span>
+          ))}
+          <span onClick={() => selectPageHandler(page + 1)}>👉</span>
+        </div>
+      )}
+    </div>
   );
 };
 
